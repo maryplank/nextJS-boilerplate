f0a2a4fbf3be4366b902cf4f09c258623f7ca270
Mariana Fernandes Ribeiro <mariana.ribeiro@pravaler.com.br>
2020-12-17 15:59:13 -0300

    updating storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,4 @@
 module.exports = {
   "stories": ['../src/components/**/stories.tsx'],
-  "addons": [
-    "@storybook/addon-actions",
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-knobs"
-  ]
+  "addons": ["@storybook/addon-essentials"]
 }
